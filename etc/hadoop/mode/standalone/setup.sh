3d17de6f8816b96f435b3ccb028fed1c62bb0924
Wojtek Szymanski <wk.szymanski@gmail.com>
2015-10-17 14:59:09 +0200

    hadoop-env.sh added

--- etc/hadoop/mode/standalone/setup.sh
+++ etc/hadoop/mode/standalone/setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+ln -sf `pwd`/hadoop-env.sh ${HADOOP_HOME}/etc/hadoop/hadoop-env.sh
 ln -sf `pwd`/core-site.xml ${HADOOP_HOME}/etc/hadoop/core-site.xml
 ln -sf `pwd`/hdfs-site.xml ${HADOOP_HOME}/etc/hadoop/hdfs-site.xml
 ln -sf `pwd`/mapred-site.xml ${HADOOP_HOME}/etc/hadoop/mapred-site.xml
